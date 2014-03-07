271bee60d3f7b85b9921b69141e4752def68e0cc
sslnustudent <ss222tb@student.lnu.se>
2014-03-07 13:24:06 +0100

    Kommit lite gran frammåt

---
+++ Lab1_4HemligaTalet/Lab1_4HemligaTalet/Default.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Lab1_4HemligaTalet.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>   
+        <h1>Gissa det hemliga talet</h1>
+        <p>Ange ett tal mellan 1 och 100</p>
+        <asp:TextBox ID="GuessTextBox" runat="server"></asp:TextBox>
+        <asp:RequiredFieldValidator ControlToValidate="GuessTextBox" ID="RequiredFieldValidator1" runat="server" ErrorMessage="Ange ett nummer mellan 1 och 100" Display="Dynamic"></asp:RequiredFieldValidator>
+        <asp:RangeValidator ControlToValidate="GuessTextBox" ID="RangeValidator1" runat="server" ErrorMessage="Ange ett nummer mellan 1 och 100" Type="Integer" MaximumValue="100" MinimumValue="1" Display="Dynamic"></asp:RangeValidator>
+        <asp:Button ID="OkButton" runat="server" Text="Button" />
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ Lab1_4HemligaTalet/Lab1_4HemligaTalet/Model/SecretNumber.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab1_4HemligaTalet.Model
+{
+    public enum Outcome
+    {
+        Indefinite,
+        Low,
+        High,
+        Correct,
+        NoMoreGuesses,
+        PreviousGuess
+    }
+
+    public class SecretNumber
+    {
+        const int MaxNumberOfGuesses = 7;
+
+        private int _number;
+
+        private List<int> _previousGuesses;
+
+        public bool CanMakeGuess
+        {
+            get
+            {
+                if (MaxNumberOfGuesses > Count)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public int Count
+        { get { return PreviousGuesses.Count; } }
+
+        public int? Number
+        {
+            get 
+            {
+                if (CanMakeGuess == true)
+                {
+                    return _number;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+                
+        }
+
+
+        public Outcome Outcome
+        { get; set; }
+
+        public List<int> PreviousGuesses
+        { get { return _previousGuesses; } }
+
+        SecretNumber()
+        {
+
+        }
+
+        public void Initialize()
+        { }
+
+        public Outcome MakeGuess(int guess)
+        {
+            return Outcome.Low;
+        }
+    }
+}
